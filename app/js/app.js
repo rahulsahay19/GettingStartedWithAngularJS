d3c79ec3a61070296e5df2f7be51a79ceb32b90c
rahul sahay <rahulsahay19@gmail.com>
2015-08-01 21:35:50 +0530

    Chapter 5 Completed

--- app/js/app.js
+++ app/js/app.js
@@ -1,34 +1,45 @@
 'use strict';
 
-var moviesApp = angular.module('moviesApp', ['ngResource','ngRoute'])
+var moviesApp = angular.module('moviesApp', ['ngResource', 'ngRoute'])
   .config(function ($routeProvider) {
-    $routeProvider.when('/',{
-      templateUrl:'templates/MovieDetails.html',
+    $routeProvider.when('/', {
+      templateUrl: 'templates/MovieDetails.html',
       controller: 'MovieController'
     });
-    $routeProvider.when('/NewMovie',{
-      templateUrl:'templates/NewMovie.html',
+    $routeProvider.when('/NewMovie', {
+      templateUrl: 'templates/NewMovie.html',
       controller: 'PostMovieController'
     });
-    $routeProvider.when('/MovieDetails',{
-      templateUrl:'templates/MovieDetails.html',
+    $routeProvider.when('/MovieDetails', {
+      templateUrl: 'templates/MovieDetails.html',
       controller: 'MovieController'
     });
-    $routeProvider.when('/EditUser',{
-      templateUrl:'templates/EditUser.html',
+    $routeProvider.when('/EditUser', {
+      templateUrl: 'templates/EditUser.html',
       controller: 'EditUserController'
     });
-    $routeProvider.when('/cachePage',{
-      templateUrl:'templates/cachePage.html',
+    $routeProvider.when('/cachePage', {
+      templateUrl: 'templates/cachePage.html',
       controller: 'cacheController'
     });
     /*$routeProvider.when('/compileSample',{
-      templateUrl:'templates/compileSample.html',
-      controller: 'compileSample'
-    });*/
-    $routeProvider.when('/show/:id',{
-      templateUrl:'templates/MovieDetail.html',
+     templateUrl:'templates/compileSample.html',
+     controller: 'compileSample'
+     });*/
+    $routeProvider.when('/show/:id', {
+      templateUrl: 'templates/MovieDetail.html',
       controller: 'MovieDetailController'
+    });
+
+    $routeProvider.when('/customPage', {
+      templateUrl: 'templates/customPage.html',
+      controller: 'customDirectiveController'
+    });
+    $routeProvider.when('/calcTime', {
+      templateUrl: 'templates/directiveController.html'
+    });
+    $routeProvider.when('/helloAngular', {
+      templateUrl: 'templates/helloAngular.html'
     });
     $routeProvider.otherwise({templateUrl: 'templates/errorPage.html'});
   });
